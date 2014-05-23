b4a59adcc74ab34b9e38a99a3774ad738a43e073
Mohammad Mousa <mohammad.riad@gmail.com>
2014-05-23 11:08:00 +0300

    Adding BeneficiaryOrphan Missing translation

--- protected/modules/admin/views/beneficiaryorphan/update.php
+++ protected/modules/admin/views/beneficiaryorphan/update.php
@@ -1,18 +1,18 @@
 <?php
 $this->breadcrumbs=array(
-	'Beneficiary Orphans'=>array('index'),
+	Yii::t('app', 'Beneficiary Orphans')=>array('index'),
 	$model->id=>array('view','id'=>$model->id),
 	Yii::t('app', 'Update'),
 );
 
 $this->menu=array(
-	array('label'=>'List BeneficiaryOrphan','url'=>array('index')),
-	array('label'=>'Create BeneficiaryOrphan','url'=>array('create')),
-	array('label'=>'View BeneficiaryOrphan','url'=>array('view','id'=>$model->id)),
-	array('label'=>'Manage BeneficiaryOrphan','url'=>array('admin')),
+	array('label'=>Yii::t('BeneficiaryOrphan', 'List Beneficiary Orphan'),'url'=>array('index')),
+	array('label'=>Yii::t('BeneficiaryOrphan', 'Create Beneficiary Orphan'),'url'=>array('create')),
+	array('label'=>Yii::t('BeneficiaryOrphan', 'View Beneficiary Orphan'),'url'=>array('view','id'=>$model->id)),
+	array('label'=>Yii::t('BeneficiaryOrphan', 'Manage Beneficiary Orphan'),'url'=>array('admin')),
 );
 ?>
 
-<h1>Update BeneficiaryOrphan <?php echo $model->id; ?></h1>
+<h1><?php echo Yii::t('BeneficiaryOrphan', 'Update Beneficiary Orphan')?> <?php echo $model->id; ?></h1>
 
 <?php echo $this->renderPartial('_form',array('model'=>$model)); ?>
