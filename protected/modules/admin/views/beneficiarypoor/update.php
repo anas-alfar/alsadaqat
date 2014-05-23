9101da3f1ad5059fe306d96bbc0f4f03a323387a
Mohammad Mousa <mohammad.riad@gmail.com>
2014-05-23 11:21:38 +0300

    Adding Missing translation for BeneficiaryPoor

--- protected/modules/admin/views/beneficiarypoor/update.php
+++ protected/modules/admin/views/beneficiarypoor/update.php
@@ -1,18 +1,18 @@
 <?php
 $this->breadcrumbs=array(
-	'Beneficiary Poors'=>array('index'),
+	Yii::t('app', 'Beneficiary Poors')=>array('index'),
 	$model->id=>array('view','id'=>$model->id),
 	Yii::t('app', 'Update'),
 );
 
 $this->menu=array(
-	array('label'=>'List BeneficiaryPoor','url'=>array('index')),
-	array('label'=>'Create BeneficiaryPoor','url'=>array('create')),
-	array('label'=>'View BeneficiaryPoor','url'=>array('view','id'=>$model->id)),
-	array('label'=>'Manage BeneficiaryPoor','url'=>array('admin')),
+	array('label'=>Yii::t('BeneficiaryPoor', 'List Beneficiary Poor'),'url'=>array('index')),
+	array('label'=>Yii::t('BeneficiaryPoor', 'Create Beneficiary Poor'),'url'=>array('create')),
+	array('label'=>Yii::t('BeneficiaryPoor', 'View Beneficiary Poor'),'url'=>array('view','id'=>$model->id)),
+	array('label'=>Yii::t('BeneficiaryPoor', 'Manage Beneficiary Poor'),'url'=>array('admin')),
 );
 ?>
 
-<h1>Update BeneficiaryPoor <?php echo $model->id; ?></h1>
+<h1><?php echo Yii::t('BeneficiaryPoor', 'Update Beneficiary Poor')?> <?php echo $model->id; ?></h1>
 
 <?php echo $this->renderPartial('_form',array('model'=>$model)); ?>
