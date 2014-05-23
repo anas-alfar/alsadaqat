367ebca6976abeba365dd23035265957441ef7cc
Mohammad Mousa <mohammad.riad@gmail.com>
2014-05-23 21:24:22 +0300

    Add echo

--- protected/modules/admin/views/organization/update.php
+++ protected/modules/admin/views/organization/update.php
@@ -13,6 +13,6 @@ $this->menu=array(
 );
 ?>
 
-<h1><?php Yii::t('organization', 'Update Organization') ?> <?php echo $model->id; ?></h1>
+<h1><?php echo Yii::t('organization', 'Update Organization') ?> <?php echo $model->id; ?></h1>
 
 <?php echo $this->renderPartial('_form',array('model'=>$model)); ?>
