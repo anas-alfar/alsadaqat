6cd4304c34fadabbaa6005ed7d3afa120ee48c52
Mohammad Mousa <mohammad.riad@gmail.com>
2014-06-21 00:04:09 +0300

    Enhance All Mail forms

--- protected/modules/admin/views/mailinbox/_form.php
+++ protected/modules/admin/views/mailinbox/_form.php
@@ -1,8 +1,24 @@
 <?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
 	'id'=>'mail-inbox-form',
-	'enableAjaxValidation'=>false,
+    'enableAjaxValidation'   => false,
+    'enableClientValidation' => true,
     'htmlOptions' => array(
         'class' => 'well',
+    ),
+    'clientOptions' => array(
+        'validateOnSubmit' => true,
+        'afterValidate'    => 'js:function(form, data, hasError) {
+            if(hasError) {
+              for(var i in data){
+                $("html, body").animate({
+                  scrollTop: $("div.error").offset().top - 100
+                 }, 1000);
+                 return false;
+              } 
+           }else{
+            return true;
+           }
+        }',
     ),
 )); ?>
 
@@ -22,18 +38,35 @@
 
 	<?php echo $form->textFieldControlGroup($model,'subject',array('class'=>'span5','maxlength'=>255)); ?>
 
-	<?php echo $form->textFieldControlGroup($model,'received_at',array('class'=>'span5')); ?>
+
+    <?php
+        $receivedAt = $this->widget('yiiwheels.widgets.datetimepicker.WhDateTimePicker', array(
+            'model' => $model,
+            'attribute' => 'received_at',
+            //'name'      => 'end_at',
+            'pluginOptions' => array(
+                'language' => (Yii::app()->language == 'ar') ? Yii::app()->language : '',
+                'format' => 'yyyy-MM-dd hh:mm:ss'
+            ),
+            'htmlOptions' => array(
+                'class' => 'span5',
+            )
+        ), TRUE);
+    ?>
+    <?php echo TbHtml::customActiveControlGroup($receivedAt, $model, 'received_at'); ?>
+    <?php $form->error($model, 'received_at')?>
+	<?php //echo $form->textFieldControlGroup($model,'received_at',array('class'=>'span5')); ?>
+
 
 	<?php echo $form->textFieldControlGroup($model,'owner_id',array('class'=>'span5','maxlength'=>11)); ?>
 
 	<?php echo $form->textFieldControlGroup($model,'mail_attachment_id',array('class'=>'span5','maxlength'=>11)); ?>
 
-	<div class="form-actions">
-		<?php $this->widget('bootstrap.widgets.TbButton', array(
-			'buttonType'=>'submit',
-			'type'=>'primary',
-			'label'=>$model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Save'),
-		)); ?>
-	</div>
+    <div class="form-actions">
+        <?php echo TbHtml::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Save'), array(
+            'color'=> TbHtml::BUTTON_COLOR_PRIMARY,
+            'size' => TbHtml::BUTTON_SIZE_LARGE,
+        )); ?>
+    </div>
 
 <?php $this->endWidget(); ?>
--- protected/modules/admin/views/mailoutbox/_form.php
+++ protected/modules/admin/views/mailoutbox/_form.php
@@ -1,8 +1,24 @@
 <?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
 	'id'=>'mail-outbox-form',
-	'enableAjaxValidation'=>false,
+    'enableAjaxValidation'   => false,
+    'enableClientValidation' => true,
     'htmlOptions' => array(
         'class' => 'well',
+    ),
+    'clientOptions' => array(
+        'validateOnSubmit' => true,
+        'afterValidate'    => 'js:function(form, data, hasError) {
+            if(hasError) {
+              for(var i in data){
+                $("html, body").animate({
+                  scrollTop: $("div.error").offset().top - 100
+                 }, 1000);
+                 return false;
+              } 
+           }else{
+            return true;
+           }
+        }',
     ),
 )); ?>
 
@@ -32,18 +48,36 @@
 
 	<?php echo $form->textFieldControlGroup($model,'subject',array('class'=>'span5','maxlength'=>255)); ?>
 
-	<?php echo $form->textAreaRow($model,'description',array('rows'=>6, 'cols'=>50, 'class'=>'span8')); ?>
+	<?php echo $form->textAreaControlGroup($model,'description',array('rows'=>6, 'cols'=>50, 'class'=>'span8')); ?>
 
-	<?php echo $form->textFieldControlGroup($model,'send_at',array('class'=>'span5')); ?>
+
+    <?php
+        $sendAt = $this->widget('yiiwheels.widgets.datetimepicker.WhDateTimePicker', array(
+            'model' => $model,
+            'attribute' => 'send_at',
+            //'name'      => 'end_at',
+            'pluginOptions' => array(
+                'language' => (Yii::app()->language == 'ar') ? Yii::app()->language : '',
+                'format' => 'yyyy-MM-dd hh:mm:ss'
+            ),
+            'htmlOptions' => array(
+                'class' => 'span5',
+            )
+        ), TRUE);
+    ?>
+    <?php echo TbHtml::customActiveControlGroup($sendAt, $model, 'send_at'); ?>
+    <?php $form->error($model, 'send_at')?>
+	<?php //echo $form->textFieldControlGroup($model,'send_at',array('class'=>'span5')); ?>
+
+
 
 	<?php echo $form->textFieldControlGroup($model,'owner_id',array('class'=>'span5','maxlength'=>11)); ?>
 
-	<div class="form-actions">
-		<?php $this->widget('bootstrap.widgets.TbButton', array(
-			'buttonType'=>'submit',
-			'type'=>'primary',
-			'label'=>$model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Save'),
-		)); ?>
-	</div>
+    <div class="form-actions">
+        <?php echo TbHtml::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Save'), array(
+            'color'=> TbHtml::BUTTON_COLOR_PRIMARY,
+            'size' => TbHtml::BUTTON_SIZE_LARGE,
+        )); ?>
+    </div>
 
 <?php $this->endWidget(); ?>
